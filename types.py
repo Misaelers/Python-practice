a1df26999358edd3ac325e479e8de194155b07fa
EC2 Default User <ec2-user@ip-172-31-11-20.ec2.internal>
2024-05-21 18:53:28 +0000

    examples types of data

---
+++ types.py
@@ -0,0 +1,18 @@
+a = 2
+b = -2.2
+c = 'linux'
+d = (2, 3, 4, 5)
+e = ['pera', 'manzana', 'piña', 'mango']
+f = True
+g = 1j
+Dicc = {'a' : 'bueno',
+    'b' : 'malo'
+}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(Dicc))
